5cf2f588916acd85a635ed5bdbcc672b0df19198
Queue <elbertq2@gmail.com>
2020-10-10 22:02:27 -0500

    branch update

--- RestaurantMenu/RestaurantMenu/Menu.cs
+++ RestaurantMenu/RestaurantMenu/Menu.cs
@@ -6,15 +6,25 @@ namespace RestaurantMenu
 {
     class Menu
     {
-//menu
+        //menu is a list of dictionaries
+        //menu class contains a method that creates the dictionary containing the values for each menu item
+        // the value for some keys will be instance methods (i.e. category, new item, date added)
+        //menu class contains a method that removes items from the menu list
+        //menu class contains a method that allows the user to print a single menu list items as well as the full menu list
 
-//includes a method that prompts user to instantiate a new MenuItem object
+        //menu item class contains a method that instantiates objects from the selected dictionary items found in the menu list
+        // all fields and methods seem to rely on data; that's specific to each object; so no static class members
+        //menu item contains a method that compares menu objects to check to see if they're equal
 
-//that returns the MenuItem's fields and values as a new dictionary
+        //==================================
 
-//each dictionary is added to the MenuList list
+        //includes a method that prompts user to instantiate a new MenuItem object
 
-//and sorted by the Category key?
+        //that returns the MenuItem's fields and values as a new dictionary
+
+        //each dictionary is added to the MenuList list
+
+        //and sorted by the Category key?
 
     }
 }
