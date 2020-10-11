9259399ee4c173fdb7609550e136ad62c221df51
Queue <elbertq2@gmail.com>
2020-10-10 19:20:26 -0500

    studio 3

---
+++ RestaurantMenu/RestaurantMenu/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantMenu
+{
+    class Menu
+    {
+//menu
+
+//includes a method that prompts user to instantiate a new MenuItem object
+
+//that returns the MenuItem's fields and values as a new dictionary
+
+//each dictionary is added to the MenuList list
+
+//and sorted by the Category key?
+
+    }
+}
---
+++ RestaurantMenu/RestaurantMenu/MenuItem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantMenu
+{
+    class MenuItem
+    {
+        private class MenuItem
+        {
+            private double price;
+            public double Price
+            {
+                get { return price; }
+                internal set { price = value; }
+            }
+            private string description;
+            public string Description
+            {
+                get { return description; }
+                internal set { description = value; }
+            }
+            private string category;
+            public string Category
+            {
+                get { return category; }
+                internal set { category = value; }
+            }
+            private string newItem;
+            public static string NewItem
+            {
+                get { return newItem; }
+                set { newItem = value; }
+            }
+            private string updated;
+            public static string Updated
+            {
+                get { return updated; }
+                internal set { updated = value; }
+            }
+        }
+
+    }
+}
